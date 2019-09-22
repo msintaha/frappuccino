aabcbbd213c12fdf7d681748c9a00888da80474a
msintaha <msintaha94@gmail.com>
2019-09-22 21:50:54 +0600

    Add bootstrap css

--- src/client/components/Header/index.jsx
+++ src/client/components/Header/index.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 function Header() {
   return (
     <div className="header">
-      <a href="/" className="brand-logo">
-        <img width="50" src="./assets/frappuccino.png" />
-      </a>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">
+              <img alt="Frappuccino" height="22" src="./assets/frappuccino.png" />
+            </a>
+            <p class="navbar-text">Frappuccino</p>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
