855f986f84576e7183e7003f3d293defd81309a2
msintaha <msintaha94@gmail.com>
2019-09-22 23:49:07 +0600

    Use async await

--- server/config/routes/user.js
+++ server/config/routes/user.js
@@ -1,10 +1,11 @@
 const authenticator = require('../middlewares/authenticator');
 const User = require('../../user');
+const utils = require('../../utils');
 
 module.exports = (app) => {
-  app.patch('/api/users/:id', authenticator.requiresLogin, User.update);
-  app.get('/api/users/:id', User.get);
-  app.get('/api/users/', User.list);
-  app.delete('/api/users/:id', authenticator.requiresLogin, User.remove);
-  app.get('/api/users/logged-in', authenticator.requiresLogin, User.getCurrentUser);
+  app.patch('/api/users/:id', authenticator.requiresLogin, utils.wrapAsync(User.update));
+  app.get('/api/users/:id', utils.wrapAsync(User.get));
+  app.get('/api/users/', utils.wrapAsync(User.list));
+  app.delete('/api/users/:id', authenticator.requiresLogin, utils.wrapAsync(User.remove));
+  app.get('/api/users/logged-in', authenticator.requiresLogin, utils.wrapAsync(User.getCurrentUser));
 };
--- server/user/index.js
+++ server/user/index.js
@@ -1,43 +1,33 @@
 const _ = require('lodash');
 const User = require('./User');
 
-function update(req, res) {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, updatedUser) => {
-    if (err) { return res.status(400).send('Error updating user.'); }
-    return res.status(200).send(updatedUser);
-  });
+async function update(req, res) {
+  const updatedUser = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+  return res.status(200).send(updatedUser);
 }
 
-function list(req, res) {
+async function list(req, res) {
   let queryObject = {};
 
   if (_.get(req.query, 'email')) { queryObject = { email: req.query.email }; }
 
-  User.find(queryObject, (err, users) => {
-    if (err) { return res.status(400).send('Error fetching users.'); }
-    return res.status(200).send(users);
-  });
+  const users = await User.find(queryObject);
+  return res.status(200).send(users);
 }
 
-function remove(req, res) {
-  User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
-    if (err) { return res.status(400).send('Failed to delete user.'); }
-    return res.status(200).send(deletedUser);
-  });
+async function remove(req, res) {
+  const deletedUser = await User.findByIdAndRemove(req.params.id);
+  return res.status(200).send(deletedUser);
 }
 
-function get(req, res) {
-  User.findOne({ _id: req.params.id }, (err, user) => {
-    if (err) { return res.status(400).send('Failed to fetch user.'); }
-    return res.status(200).send(user);
-  });
+async function get(req, res) {
+  const user = await User.findOne({ _id: req.params.id });
+  return res.status(200).send(user);
 }
 
-function getCurrentUser(req, res) {
-  User.findById(req.session.user._id, (err, user) => {
-    if (err || !user) { return res.status(400).send('Error fetching user.'); }
-    return res.status(200).send(user);
-  });
+async function getCurrentUser(req, res) {
+  const currentUser = await User.findById(req.session.user._id);
+  return res.status(200).send(currentUser);
 }
 
 module.exports = {
