3b7895a3c85ba651cd1909827dfd87ab4ecdc028
msintaha <msintaha94@gmail.com>
2020-05-01 20:46:05 +0600

    Add travis file

--- server/keys.js
+++ server/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  mongoHost: process.env.MONGOHOST,
-  mongoDbName: process.env.MONGODB_NAME,
-  mongoTestDbName: process.env.MONGODB_TEST,
-  mongoPort: process.env.MONGOPORT,
+  mongoHost: process.env.MONGOHOST || 'localhost',
+  mongoDbName: process.env.MONGODB_NAME || 'frappuccino',
+  mongoTestDbName: process.env.MONGODB_TEST || 'frappuccino_test',
+  mongoPort: process.env.MONGOPORT || '27017',
   expressSessionKey: process.env.EXPRESS_SESSION_KEY,
   expressSessionSecret: process.env.EXPRESS_SESSION_SECRET,
 };
